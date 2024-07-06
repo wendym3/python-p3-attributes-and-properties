95527ade8d29df5b82490d44dec2cd8d81b8399c
wendymasese <wendy.masese@student.moringaschool.com>
2024-07-06 17:19:02 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,32 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Dog", breed="Pug"):
+        self._name = None
+        self._breed = None
+        self.name = name  # This will use the setter method
+        self.breed = breed  # This will use the setter method
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and (1 <= len(name) <= 25):
+            self._name = name
+        else:
+            self._name = None
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            self._breed = None
+            print("Breed must be in list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 APPROVED_JOBS = [
     "Admin",
     "Customer Service",
@@ -16,4 +14,33 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    approved_jobs = APPROVED_JOBS  
+
+    def __init__(self, name="Unknown", job="Admin"):
+        self._name = None
+        self._job = None
+        self.name = name  
+        self.job = job  
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and (1 <= len(name) <= 25):
+            self._name = name.title()  
+        else:
+            self._name = None
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, job):
+        if job in Person.approved_jobs: 
+            self._job = job
+        else:
+            self._job = None
+            print("Job must be in list of approved jobs.")
